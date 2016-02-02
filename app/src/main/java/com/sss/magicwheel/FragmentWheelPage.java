a569e8ce1b50c8b9406d9489401835ad866608d8
Alexey <alsifero@gmail.com>
2016-02-02 13:50:59 +0200

    WheelOfFortune: wheel config adjusted to fit screen size

--- app/src/main/java/com/sss/magicwheel/FragmentWheelPage.java
+++ app/src/main/java/com/sss/magicwheel/FragmentWheelPage.java
@@ -34,18 +34,30 @@ public final class FragmentWheelPage extends Fragment {
 
     private static final int DEFAULT_SECTOR_ANGLE_IN_DEGREE = 20;
 
+    private boolean isWheelContainerInitialized;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        WheelComputationHelper.initialize(createCircleConfig());
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container, Bundle savedInstanceState) {
 
         final View rootView = inflater.inflate(R.layout.fragment_wheel_page_layout, container, false);
         final RecyclerView wheelContainerView = (RecyclerView) rootView.findViewById(R.id.wheel_container);
-        initWheelContainer(wheelContainerView);
 
         rootView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
-                final int containerHeight = ((MainActivity) getActivity()).getAvailableSpace();
+
+                if (!isWheelContainerInitialized) {
+                    isWheelContainerInitialized = true;
+                    final int fragmentContainerTopEdge = container.getTop();
+                    Log.e("TAG", "container.getTop() [" + fragmentContainerTopEdge + "]");
+
+                    WheelComputationHelper.initialize(createCircleConfig(fragmentContainerTopEdge));
+                    initWheelContainer(wheelContainerView);
+                }
+
+
+
+//                final int containerHeight = ((MainActivity) getActivity()).getAvailableSpace();
             }
         });
 
@@ -76,20 +88,19 @@ public final class FragmentWheelPage extends Fragment {
         return Collections.unmodifiableList(items);
     }
 
-    private WheelConfig createCircleConfig() {
+    private WheelConfig createCircleConfig(int fragmentContainerTopEdge) {
         WindowManager wm = (WindowManager) getActivity().getSystemService(Context.WINDOW_SERVICE);
 
-        Point screenSize = calcScreenSize(wm.getDefaultDisplay());
-        final int screenWidth = screenSize.x;
-        final int screenHeight = screenSize.y;
+        final int screenHeight = getScreenHeight(wm.getDefaultDisplay());
 
         Log.e("TAG", "Sh Fragment [" + screenHeight + "]");
 
-        final PointF circleCenter = new PointF(0, screenHeight / 2 );
+        final int yWheelCenterPosition = (screenHeight - fragmentContainerTopEdge) / 2 ;
+        final PointF circleCenter = new PointF(0, yWheelCenterPosition);
 
         // TODO: 03.12.2015 Not good hardcoded values
-        final int outerRadius =  screenHeight / 2;
-        final int innerRadius = outerRadius - 200;
+        final int outerRadius =  (screenHeight - fragmentContainerTopEdge) / 2;
+        final int innerRadius = outerRadius - 300;
 
         WheelConfig.AngularRestrictions angularRestrictions = new WheelConfig.AngularRestrictions(
                 WheelComputationHelper.degreeToRadian(DEFAULT_SECTOR_ANGLE_IN_DEGREE),
@@ -101,10 +112,10 @@ public final class FragmentWheelPage extends Fragment {
     }
 
     // TODO: 03.12.2015 Wheel boundaries might be not restricted by screen but by custom rectangle instead
-    private Point calcScreenSize(Display display) {
+    private int getScreenHeight(Display display) {
         Point size = new Point();
         display.getSize(size);
-        return size;
+        return size.y;
     }
 
 }
